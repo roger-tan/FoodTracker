3b7dda8cc092b85cea38df7c7a0697a7b34a1cb7
Roger TAN <mail+github@rogertan.fr>
2017-05-29 15:35:09 +0100

    - Adds Unit Tests - Implements Danger

--- FoodTrackerTests/Models/ProductTests.swift
+++ FoodTrackerTests/Models/ProductTests.swift
@@ -11,17 +11,45 @@ import XCTest
 
 class ProductTests: XCTestCase {
     
+    // MARK: - Store Properties
+    
     var subject: Product!
+    
+    // MARK: - XCTests
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        guard let json = FileHelper.loadDataFromJSON(for: "ProductResponse") else {
+            XCTFail()
+            return
+        }
+        
+        do {
+            let productResponse = try ProductResponse(dictionary: json)
+            subject = productResponse.product
+        } catch {
+            XCTFail()
+        }
+        
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
+    // MARK: - Unit Tests
+    
+    func testProduct_whenInitializedWithDictionary_matchesName() {
+        XCTAssertEqual(subject.name, "Lardons fumés de qualité supérieure")
+    }
+    
+    func testProduct_whenInitializedWithDictionary_matchesBrand() {
+        XCTAssertEqual(subject.brand, "Herta")
+    }
+    
+    func testProduct_whenInitializedWithDictionary_matchesCountries() {
+        XCTAssertEqual(subject.countries, "France")
+    }
 
 }
